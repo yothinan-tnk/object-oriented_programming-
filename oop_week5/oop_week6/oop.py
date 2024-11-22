fd523776663c35c2d35cd90c7026db1b25422a23
yothinan-tnk <67319010033@tnk.ac.th>
2024-11-22 16:31:44 +0700

    oop

---
+++ oop_week5/oop_week6/oop.py
@@ -0,0 +1,10 @@
+class Cat:
+    def __init__(self, name, age, color, hungry):
+        self.name  = ชื่อ
+        self.age = อายุ
+        self.color = สี
+        self.hungry = หิว
+    def eat(self, food):
+        self.hungry += food
+cat1 = Cat(ชื่อ="ศรีทอง", อายุ=10, สี="ส้ม", หิว=5)
+cat2 = Cat(ชื่อ="ศรีเงิน", อายุ=8, สี="ขาว", หิว=1)
